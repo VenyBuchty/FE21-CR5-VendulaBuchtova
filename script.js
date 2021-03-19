6dc88db50be5607213519565f6487182650bca75
Vendula Buchtova <ven.buchtova@gmail.com>
2021-03-19 21:18:09 +0100

    regular done

--- script.js
+++ script.js
@@ -23,18 +23,18 @@ var Locations = /** @class */ (function () {
         this.image = image;
     }
     Locations.prototype.display = function () {
-        var cardCreator = "<div class=\"col card-container\" style=\"height: 32rem;\">\n                                <div class=\"card\" style=\"width: 18rem; height: 31rem;\">\n                                    <img src=\"" + this.image + "\" class=\"card-img-top\" alt=\"...\" style=\"object-fit: cover; height: 12rem;\">\n                                <div class=\"card-body\">\n                                    <h5 class=\"card-title\">" + this.name + "</h5>\n                                    <p class=\"card-text\">" + this.city + "</p>\n                                    <p class=\"card-text\">" + this.zip_code + "</p>\n                                    <p class=\"card-text\">" + this.address + "</p>\n                            ";
+        var cardCreator = "<div class=\"col card-container\">\n                                    <div class=\"card\" style=\"width: 18rem;\">\n                                        <img src=\"" + this.image + "\" class=\"card-img-top\" alt=\"...\" style=\"object-fit: cover; height: 12rem;\">\n                                    <div class=\"card-body\">\n                                        <div class=\"d-flex flex-column justify-content-start\">\n                                            <h4 class=\"card-title text-center\">" + this.name + "</h4>\n                                        </div>\n                                        <div class=\"d-flex flex-column\">\n                                                    <span class=\"badge color-badge\"><i class=\"fas fa-map-marker-alt\"></i> Address:</span>\n                                                    <p class=\"card-text\">" + this.city + "</p>\n                                                    <p class=\"card-text\">" + this.zip_code + "</p>\n                                                    <p class=\"card-text\">" + this.address + "</p>\n                            ";
         return cardCreator;
     };
     Locations.prototype.closeDiv = function () {
-        return "                  </div>\n                            </div>\n                        </div>";
+        return "                        </div>\n                                </div>\n                            </div>\n                        </div>";
     };
     return Locations;
 }());
 var locationsArr = [
     new Locations("Palmenhaus Schönbrunn", "Vienna", 1130, "Schönbrunner Schloßstraße 47", "./img/place1.jpg"),
     new Locations("Walt Disney Concert Hall", "Los Angeles", 90012, "111 S Grand Ave", "./img/place2.jpg"),
-    new Locations("Museum Montanelli", "Praha", 11015, "Staroměstské nám. 1/12", "./img/place3.jpg"),
+    new Locations("Museum Montanelli Bocelo", "Praha", 11015, "Staroměstské nám. 1/12", "./img/place3.jpg"),
     new Locations("Parc zoologique de Paris", "Paris", 75012, " Avenue Daumesnil", "./img/place4.jpg"),
 ];
 for (var _i = 0, locationsArr_1 = locationsArr; _i < locationsArr_1.length; _i++) {
@@ -52,16 +52,16 @@ var Restaurant = /** @class */ (function (_super) {
         return _this;
     }
     Restaurant.prototype.display = function () {
-        var cardCreator = " " + _super.prototype.display.call(this) + "\n                                    <p class=\"card-text\">" + this.tel_number + "</p>\n                                    <p class=\"card-text\">" + this.cuisine + "</p>\n                                    <p class=\"card-text\">" + this.web_address + "</p>\n                                </div>       \n                            </div>\n                        </div>";
+        var cardCreator = " " + _super.prototype.display.call(this) + "\n                                    <span class=\"badge color-badge\"><i class=\"fas fa-phone\"></i> Telephone:</span>\n                                    <p class=\"card-text\">" + this.tel_number + "</p>\n                                    <span class=\"badge color-badge\"><i class=\"fas fa-utensils\"></i> Cuisine:</span>\n                                    <p class=\"card-text\">" + this.cuisine + "</p>\n                                    <span class=\"badge color-badge\"><i class=\"fas fa-globe\"></i> Web page:</span>\n                                    <p class=\"card-text\">" + this.web_address + "</p>\n                                </div>       \n                            </div>\n                        </div>";
         return cardCreator;
     };
     return Restaurant;
 }(Locations));
 var restaurantArr = [
-    new Restaurant("Osteria Francescana", "Modena", 41121, "Via Stella, 22", "./img/res1.jpg", 660834234, "indian", "skapidor@gmail.com"),
-    new Restaurant("El Celler de Can Roca", "Girona", 17007, "Carrer de Can Sunyer, 48", "./img/res2.jpg", 4354363463, "czech", "karolas@gmail.com"),
-    new Restaurant("Mirazur", "Menton", 6500, "30 Avenue Aristide Briand", "./img/res3.jpg", 823395432, "chinese", "kungpao@mail.com"),
-    new Restaurant("The Ledbury", "Nothing Hill, London", 1120, "127 Ledbury Rd", "./img/res4.jpg", 233358102, "polish", "pirogy@mail.com"),
+    new Restaurant("Osteria Francescana", "Modena", 41121, "Via Stella, 22", "./img/res1.jpg", 660834234, "american", "osterFrances@gmail.com"),
+    new Restaurant("El Celler de Roca", "Girona", 17007, "Carrer de Can Sunyer, 48", "./img/res2.jpg", 4354363463, "spanish", "cellerRoca@gmail.com"),
+    new Restaurant("Mirazur", "Menton", 6500, "30 Avenue Aristide Briand", "./img/res3.jpg", 823395432, "oriental", "mirazur@mail.com"),
+    new Restaurant("The Ledbury", "Nothing Hill, London", 1120, "127 Ledbury Rd", "./img/res4.jpg", 233358102, "rustical", "ledBurys@mail.com"),
 ];
 for (var _a = 0, restaurantArr_1 = restaurantArr; _a < restaurantArr_1.length; _a++) {
     var value = restaurantArr_1[_a];
@@ -78,16 +78,16 @@ var Events = /** @class */ (function (_super) {
         return _this;
     }
     Events.prototype.display = function () {
-        var cardCreator = " " + _super.prototype.display.call(this) + "\n                                    <p class=\"card-text\">" + this.event_date + "</p>\n                                    <p class=\"card-text\">" + this.event_time + "</p>\n                                    <p class=\"card-text\">" + this.ticket_price + "</p>\n                                </div>       \n                            </div>\n                        </div>";
+        var cardCreator = " " + _super.prototype.display.call(this) + "\n                                    <span class=\"badge color-badge\"><i class=\"fas fa-calendar-week\"></i> Event Date:</span>\n                                    <p class=\"card-text\">" + this.event_date + "</p>\n                                    <p class=\"card-text\">" + this.event_time + "</p>\n                                    <span class=\"badge color-badge\"><i class=\"fas fa-euro-sign\"></i> Price:</span>\n                                    <p class=\"card-text\">" + this.ticket_price + "</p>\n                                </div>       \n                            </div>\n                        </div>";
         return cardCreator;
     };
     return Events;
 }(Locations));
 var eventsArr = [
-    new Events("DJ TemeraF", "Belfort", 14424, "Les Eurockeennes", "./img/event1.jpg", "01.07.2021", "21:00", "120 €"),
+    new Events("DJ TemeraF", "Belfort", 14424, "Club DiAziO, Eurockeennes 173", "./img/event1.jpg", "01.07.2021", "21:00", "120 €"),
     new Events("Spring Food Festival", "Budapest", 1093, "Vámház krt. 1-3", "./img/event2.jpg", "04.04.2021", "09:00", "3 €"),
-    new Events("Schwanensee", "Vienna", 1010, "Opernring 2", "./img/event3.jpg", "12.05.2021", "20:00", "203 €"),
-    new Events("Zucchero", "Milano", 57391, "Stadium Theatre", "./img/event4.jpg", "14.08.2021", "19:00", "89 €"),
+    new Events("Schwanensee", "Vienna", 1010, "Vienna Oper, Opernring 2", "./img/event3.jpg", "12.05.2021", "20:00", "203 €"),
+    new Events("Zucchero", "Milano", 57391, "Stadium Theatre 2", "./img/event4.jpg", "14.08.2021", "19:00", "89 €"),
 ];
 for (var _b = 0, eventsArr_1 = eventsArr; _b < eventsArr_1.length; _b++) {
     var value = eventsArr_1[_b];
--- script.ts
+++ script.ts
@@ -16,20 +16,25 @@ class Locations {
         }
 
     display() : string {
-    let cardCreator : string = `<div class="col card-container" style="height: 32rem;">
-                                <div class="card" style="width: 18rem; height: 31rem;">
-                                    <img src="${this.image}" class="card-img-top" alt="..." style="object-fit: cover; height: 12rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">${this.name}</h5>
-                                    <p class="card-text">${this.city}</p>
-                                    <p class="card-text">${this.zip_code}</p>
-                                    <p class="card-text">${this.address}</p>
+    let cardCreator : string = `<div class="col card-container">
+                                    <div class="card" style="width: 18rem;">
+                                        <img src="${this.image}" class="card-img-top" alt="..." style="object-fit: cover; height: 12rem;">
+                                    <div class="card-body">
+                                        <div class="d-flex flex-column justify-content-start">
+                                            <h4 class="card-title text-center">${this.name}</h4>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                                    <span class="badge color-badge"><i class="fas fa-map-marker-alt"></i> Address:</span>
+                                                    <p class="card-text">${this.city}</p>
+                                                    <p class="card-text">${this.zip_code}</p>
+                                                    <p class="card-text">${this.address}</p>
                             `
     return cardCreator;
     }
 
     closeDiv(){
-    return `                  </div>
+    return `                        </div>
+                                </div>
                             </div>
                         </div>`
     }
@@ -39,7 +44,7 @@ class Locations {
 let locationsArr : Array <Locations> = [
     new Locations("Palmenhaus Schönbrunn", "Vienna", 1130, "Schönbrunner Schloßstraße 47", "./img/place1.jpg"),
     new Locations("Walt Disney Concert Hall", "Los Angeles", 90012, "111 S Grand Ave", "./img/place2.jpg"),
-    new Locations("Museum Montanelli","Praha", 11015, "Staroměstské nám. 1/12", "./img/place3.jpg"),
+    new Locations("Museum Montanelli Bocelo","Praha", 11015, "Staroměstské nám. 1/12", "./img/place3.jpg"),
     new Locations("Parc zoologique de Paris", "Paris", 75012, " Avenue Daumesnil", "./img/place4.jpg"),
 ]
 
@@ -67,8 +72,11 @@ class Restaurant extends Locations {
 
     display() : string {
     let cardCreator : string = ` ${super.display()}
+                                    <span class="badge color-badge"><i class="fas fa-phone"></i> Telephone:</span>
                                     <p class="card-text">${this.tel_number}</p>
+                                    <span class="badge color-badge"><i class="fas fa-utensils"></i> Cuisine:</span>
                                     <p class="card-text">${this.cuisine}</p>
+                                    <span class="badge color-badge"><i class="fas fa-globe"></i> Web page:</span>
                                     <p class="card-text">${this.web_address}</p>
                                 </div>       
                             </div>
@@ -79,10 +87,10 @@ class Restaurant extends Locations {
 }
 
 let restaurantArr : Array <Restaurant> = [
-    new Restaurant("Osteria Francescana", "Modena", 41121, "Via Stella, 22", "./img/res1.jpg", 660834234, "indian", "skapidor@gmail.com"),
-    new Restaurant("El Celler de Can Roca", "Girona", 17007, "Carrer de Can Sunyer, 48", "./img/res2.jpg", 4354363463, "czech", "karolas@gmail.com"),
-    new Restaurant("Mirazur", "Menton", 6500, "30 Avenue Aristide Briand", "./img/res3.jpg", 823395432, "chinese", "kungpao@mail.com"),
-    new Restaurant("The Ledbury", "Nothing Hill, London", 1120, "127 Ledbury Rd", "./img/res4.jpg", 233358102, "polish", "pirogy@mail.com"),
+    new Restaurant("Osteria Francescana", "Modena", 41121, "Via Stella, 22", "./img/res1.jpg", 660834234, "american", "osterFrances@gmail.com"),
+    new Restaurant("El Celler de Roca", "Girona", 17007, "Carrer de Can Sunyer, 48", "./img/res2.jpg", 4354363463, "spanish", "cellerRoca@gmail.com"),
+    new Restaurant("Mirazur", "Menton", 6500, "30 Avenue Aristide Briand", "./img/res3.jpg", 823395432, "oriental", "mirazur@mail.com"),
+    new Restaurant("The Ledbury", "Nothing Hill, London", 1120, "127 Ledbury Rd", "./img/res4.jpg", 233358102, "rustical", "ledBurys@mail.com"),
 ]
 
 for (let value of restaurantArr) {
@@ -107,8 +115,10 @@ class Events extends Locations {
 
     display() : string {
     let cardCreator : string = ` ${super.display()}
+                                    <span class="badge color-badge"><i class="fas fa-calendar-week"></i> Event Date:</span>
                                     <p class="card-text">${this.event_date}</p>
                                     <p class="card-text">${this.event_time}</p>
+                                    <span class="badge color-badge"><i class="fas fa-euro-sign"></i> Price:</span>
                                     <p class="card-text">${this.ticket_price}</p>
                                 </div>       
                             </div>
@@ -119,10 +129,10 @@ class Events extends Locations {
 }
 
 let eventsArr : Array <Events> = [
-    new Events("DJ TemeraF", "Belfort", 14424, "Les Eurockeennes", "./img/event1.jpg", "01.07.2021", "21:00", "120 €"),
+    new Events("DJ TemeraF", "Belfort", 14424, "Club DiAziO, Eurockeennes 173", "./img/event1.jpg", "01.07.2021", "21:00", "120 €"),
     new Events("Spring Food Festival", "Budapest", 1093, "Vámház krt. 1-3", "./img/event2.jpg", "04.04.2021", "09:00", "3 €"),
-    new Events("Schwanensee", "Vienna", 1010, "Opernring 2", "./img/event3.jpg", "12.05.2021", "20:00", "203 €"),
-    new Events("Zucchero", "Milano", 57391, "Stadium Theatre", "./img/event4.jpg", "14.08.2021", "19:00", "89 €"),
+    new Events("Schwanensee", "Vienna", 1010, "Vienna Oper, Opernring 2", "./img/event3.jpg", "12.05.2021", "20:00", "203 €"),
+    new Events("Zucchero", "Milano", 57391, "Stadium Theatre 2", "./img/event4.jpg", "14.08.2021", "19:00", "89 €"),
 ]
 
 for (let value of eventsArr) {
